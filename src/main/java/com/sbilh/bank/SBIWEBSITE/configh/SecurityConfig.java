6239cce61cee8cbf2f9fb79de6d58a6d833187ff
phearin-yeun <phearinyeun2019@gmail.com>
2022-12-21 17:18:17 +0700

    upload

--- src/main/java/com/sbilh/bank/SBIWEBSITE/configh/SecurityConfig.java
+++ src/main/java/com/sbilh/bank/SBIWEBSITE/configh/SecurityConfig.java
@@ -2,10 +2,14 @@ package com.sbilh.bank.SBIWEBSITE.configh;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -15,37 +19,74 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-//    @Bean
-//    public InMemoryUserDetailsManager registerDetailMange(){
-//        UserDetails user = User.withUsername("user")
-//                .password(passwordEncoder() .encode("userPas"))
+//    public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
+//        UserDetails user = User.withUsername("admin")
+//                .password(passwordEncoder().encode("userPass"))
 //                .roles("USER")
 //                .build();
 //        UserDetails admin = User.withUsername("admin")
-//                .password(passwordEncoder() .encode("adminPass"))
+//                .password(passwordEncoder().encode("adminPass"))
 //                .roles("ADMIN")
 //                .build();
 //        return new InMemoryUserDetailsManager(user, admin);
 //    }
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user = User.withUsername("admin")
+                .password(passwordEncoder().encode("userPass"))
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withUsername("admina")
+                .password(passwordEncoder().encode("adminPass"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user,admin);
+    }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception{
+    public SecurityFilterChain filterChain (HttpSecurity http) throws Exception{
+//        http
+//                .csrf().disable()
+//                .authorizeHttpRequests()
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin()
+//                .loginPage("/login.html")
+//                .loginProcessingUrl("/perform_login")
+//                .defaultSuccessUrl("/homepage.html",true)
+//                .failureUrl("/login.html?error=true");
         http
-                .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/mobile/**", "/branches/**", "/carrer/**", "/register/**", "/news/**", "/financial/**")
-                .permitAll()
+                .antMatchers("/login**","/resource**").permitAll()
+                .anyRequest()
+                .authenticated()
                 .and()
-                .formLogin()
-                .loginPage("/login.html")
-                .loginProcessingUrl("/perform_login")
-                .defaultSuccessUrl("/homepage.html",true)
-                .failureUrl("/login.html?error=true");
-    return http.build();
+                .formLogin(Customizer.withDefaults())
+                .formLogin(loginConfig -> {
+                    loginConfig
+                            .loginPage("/login.html")
+                            .defaultSuccessUrl("/");
+                })
+                .logout().deleteCookies("JSESSIONID")
+                .logoutSuccessUrl("/login?logout");
+        return http.build();
+    }
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        return web -> web.ignoring()
+                .antMatchers("/static/**")
+                .antMatchers("/favicon.ico")
+                .antMatchers("/assets/**")
+                .antMatchers("/css/**")
+                .antMatchers("/img/**")
+                .antMatchers("/js**")
+                .antMatchers("/admin/**")
+                .antMatchers("/webjars/**")
+                .antMatchers("/templates/**");
     }
 }
--- src/main/java/com/sbilh/bank/SBIWEBSITE/repository/LoginRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sbilh.bank.SBIWEBSITE.repository;
-
-import com.sbilh.bank.SBIWEBSITE.model.LoginModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LoginRepository extends JpaRepository<LoginModel, Long> {
-}
